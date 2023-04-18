17aea0354feb48bf46bd95aab3537578e29ad7bf
SeemaPatel29 <seemahpatel29@gmail.com>
2023-04-18 14:31:57 +0100

    com-orangehrmlive-sw-2

---
+++ src/testsuite/ForgotPasswordTest.java
@@ -0,0 +1,34 @@
+package testsuite;
+
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class ForgotPasswordTest extends BaseTest {
+    String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+
+    @Before
+    public void setup() {
+        openBrowser(baseUrl);
+    }
+
+    @Test
+    public void userShouldNavigateToForgotPasswordPageSuccessfully() {
+
+        // Find forgotten password link and click on forgotten link
+        driver.findElement(By.xpath("//p[contains(@class,'forgot')]")).click();
+        String expectedMessage = "Reset Password";
+        WebElement actualTextMessage = driver.findElement(By.xpath("//h6[contains(@class,'oxd')]"));
+        String actualMessage = actualTextMessage.getText();
+        Assert.assertEquals(expectedMessage,actualMessage);
+
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+}
